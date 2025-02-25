edc877867659e2688faa4f50ec3c806939195823
Georgiy Karev <karev.g.a@mail.ru>
2025-02-25 18:35:04 +0300

    Add files via upload

---
+++ ammo_VS.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <fstream>
+using namespace std;
+
+/*class Transport {
+protected:
+	float speed;
+};
+
+class Auto : public Transport {
+private:
+	int wheels;
+
+public:
+	Auto(float speed, int wheels) {
+		this->speed = speed;
+		this->wheels = wheels;
+		cout << "Speed is: " << speed << ", model is - " << wheels << endl;
+	}
+};
+
+int main() {
+	Auto zxc(305, 4);
+	Auto audi(1488, 4);
+
+	return 0;
+}
+*/ 
+
+class Firewall {
+private: 
+	string name;
+	int lvl;
+public: 
+
+};
